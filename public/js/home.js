a5de13d693d931fbf95e7b89bd967df8189bde6c
Bruno <mourabrunob@gmail.com>
2022-04-06 10:38:57 -0300

    abril 6 novas alterações

--- public/js/home.js
+++ public/js/home.js
@@ -39,7 +39,7 @@ document.getElementById("transaction-form").addEventListener("submit" , function
     alert("Lançamento adicionado com sucesso");
 });
 
-function checkLogged();{
+function checkLogged(){
   if(session){
        sessionStorage.setItem("logged" , session);
       logged = session;
@@ -79,29 +79,29 @@ function getCashIn(){
         }else{
             limit = cashIn.length;
         }
-    }
-
-    for (let index = 0; index < limit; index++) {
-        cashInHtml +=`
-        <div class="row mb-4">
-            <div class="col-12">
-                <h3 class="fs-2">${cashIn[index].value.toFixed(2)}</h3>
-                <div class="container p-0">
-                    <div class="row">
-                        <div class="col-12 col-md-8">
-                            <p>${ cashIn[index].description}</p>                                                        
+        for (let index = 0; index < limit; index++) {
+            cashInHtml +=`
+            <div class="row mb-4">
+                <div class="col-12">
+                    <h3 class="fs-2">${cashIn[index].value.toFixed(2)}</h3>
+                    <div class="container p-0">
+                        <div class="row">
+                            <div class="col-12 col-md-8">
+                                <p>${ cashIn[index].description}</p>                                                        
+                            </div>
+                            <div class="col-12 col-md-3 d-flex justify-content-end">
+                            <span>${cashIn[index].date}</span> 
+                            </div>                                                
                         </div>
-                        <div class="col-12 col-md-3 d-flex justify-content-end">
-                        <span>${cashIn[index].date}</span> 
-                        </div>                                                
                     </div>
                 </div>
             </div>
-        </div>
-        
-        `       
+            
+            `       
+        }
+        document.getElementById("cash-in-list").innerHTML = cashInHtml;
     }
-    document.getElementByIdO("cash-in-list").innerHTML = cashInHtml;
+
    
 
 
@@ -122,29 +122,29 @@ function getCashOut(){
         }else{
             limit = cashOut.length;
         }
-    }
-
-    for (let index = 0; index < limit; index++) {
-        cashOutHtml +=`
-        <div class="row mb-4">
-            <div class="col-12">
-                <h3 class="fs-2">${cashOut[index].value.toFixed(2)}</h3>
-                <div class="container p-0">
-                    <div class="row">
-                        <div class="col-12 col-md-8">
-                            <p>${ cashOut[index].description}</p>                                                        
+        for (let index = 0; index < limit; index++) {
+            cashOutHtml +=`
+            <div class="row mb-4">
+                <div class="col-12">
+                    <h3 class="fs-2">${cashOut[index].value.toFixed(2)}</h3>
+                    <div class="container p-0">
+                        <div class="row">
+                            <div class="col-12 col-md-8">
+                                <p>${ cashOut[index].description}</p>                                                        
+                            </div>
+                            <div class="col-12 col-md-3 d-flex justify-content-end">
+                            <span>${cashOut[index].date}</span>
+                            </div>                                                
                         </div>
-                        <div class="col-12 col-md-3 d-flex justify-content-end">
-                        <span>${cashOut[index].date}</span>
-                        </div>                                                
                     </div>
                 </div>
             </div>
-        </div>
-        
-        `       
+            
+            `       
+        }
+        document.getElementById("cash-out-list").innerHTML = cashOutHtml;
     }
-    document.getElementById("cash-out-list").innerHTML = cashOutHtml;
+
    
 
 
